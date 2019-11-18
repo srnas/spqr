5931f7513741f922021baefbb79b353e325a6321
spoblete <spoblete@dlab.cl>
2019-11-18 03:49:34 -0300

    Doc and new scripts

--- tools/src/mc_end.c
+++
@@ -1,4 +0,0 @@
-#include "mc_end.h"
-
-void MC_end(int mc_n, double *rx, double *ry, double *rz, double final_time, double energy_t, int mpi_id){
-}
--- tools/tools/assemble/do.bash
+++
@@ -1,6 +0,0 @@
-SEQ=$1
-SST=$2
-python gen_src/fullassemble.py -s $SEQ -t $SST > init.pdb
-while read p ; do
-python gen_src/assemble.py -d $p >> ermsd_frags.lst    
-done < gen_src/ss.temp
