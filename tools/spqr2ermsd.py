7b4477b25d822d059ce9aa4d76f4d4f480decfbf
spoblete <spoblete@dlab.cl>
2020-08-09 01:07:39 -0400

    patched spqr2ermsd

--- tools/spqr2ermsd.py
+++ tools/spqr2ermsd.py
@@ -10,13 +10,19 @@ RERMSD=15
 
 parser=argparse.ArgumentParser()
 parser.add_argument("-i","--input", help="Input file",type=str,default="")
-parser.add_argument("-o","--output", help="Output file",type=str,default="bbm_output.pdb")
+parser.add_argument("-o","--output", help="Output file",type=str,default="out_ermsd_frags.lst")
 parser.add_argument("-t","--sstruct", help="Secondary structure",type=str,default="")
+parser.add_argument("-r","--onlystems", help="Only stems",action="store_true")
+parser.add_argument("-e","--ensurestems", help="Ensure integrity of stems",action="store_true")
+
+
 args=parser.parse_args()
 ssfile=open(args.sstruct,"r")
 ssfile.readline()
 SEQ=ssfile.readline().strip()
 rawSSTR=list(ssfile.readline().strip())
+ONLYSS=args.onlystems
+ENSUSS=args.ensurestems
 
 for nt in xrange(0,len(rawSSTR)):
     if rawSSTR[nt]!="." and rawSSTR[nt]!=")" and rawSSTR[nt]!="(":
@@ -41,8 +47,32 @@ for br in SSDICT:
                 rawSSTR[pnt]="."
                 restart=True
                 break
+ssnts=[]
+bpairs=sorted(bpairs)
+for nt in bpairs:
+    ssnts.append(nt[0])
+    ssnts.append(nt[1])
 
-
+##
+#SSSTACKS=[]
+#CSTACK=[bpairs[0]]
+#for bp in xrange(1,len(bpairs)):
+#    if(bpairs[bp][0]==bpairs[bp-1][0]+1 and bpairs[bp][1]==bpairs[bp-1][1]-1):
+#        CSTACK.append(bpairs[bp])
+#    else:
+#        if(len(CSTACK)>0):
+#            SSSTACKS.append(CSTACK)
+#        CSTACK=[bpairs[bp]]
+#if(len(CSTACK)>0):
+#    SSSTACKS.append(CSTACK)
+#STACKSEQS=[]
+#stra=[]
+#for ST in xrange(0,len(SSSTACKS)):
+#    stra=[]
+#    for nt in xrange(0,len(SSSTACKS[ST])):
+#        stra.append(rawseq[SSSTACKS[ST][nt][0]])
+#    STACKSEQS.append(stra)  
+##########
 
 INPUTFILE=args.input
 if INPUTFILE=="":
@@ -60,22 +90,36 @@ NATS=len(ALLFILE)
 NNT=NATS/NATNT
 
 #order is base, pucker, glyc - as usual: puck 3,2=0,1; glyc A,H,S=0,1,2
+NTLIST=range(0,len(ALLFILE)/NATNT)
+if(ONLYSS):
+    NTLIST=sorted(ssnts)
 
 COUNTAT=0
 OUTFILE=open(ERMSDFILE,"w")
 orig_stdout=sys.stdout
 sys.stdout=OUTFILE
 print "REMARK ERMSD PARAMS 1 " + str(RERMSD)
-stp="REMARK ERMSD SSPAIRS " + str(KSSERMSD)
-for pair in bpairs:
-    stp=stp+" " +str(pair[0])+" "+str(pair[1])
-print stp
+
+if ENSUSS:
+    stp="REMARK ERMSD SSPAIRS " + str(KSSERMSD)
+    for pair in bpairs:
+        stp=stp+" " +str(pair[0])+" "+str(pair[1])
+    print stp
+
 stp="REMARK ERMSD GROUP " + str(KERMSD)
-for nt in xrange(0,len(ALLFILE)/NATNT):
+
+
+
+for nt in NTLIST:
     stp=stp+" "+str(nt)
 print stp
-for at in xrange(0,len(ALLFILE)):
-    print ALLFILE[at].strip()
+for nt in NTLIST:
+    for at in xrange(0,NATNT):
+        print ALLFILE[nt*NATNT+at].strip()
+#for at in xrange(0,len(ALLFILE)):
+#    print ALLFILE[at].strip()
+    
+
 sys.stdout=orig_stdout
 OUTFILE.close()
-#print "ENDMDL"
+
